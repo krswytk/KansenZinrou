386552c73bcae0e4467fa60df130b37db1de87de
50arashikenta <50arashikenta@gmail.com>
2020-10-25 01:44:52 +0900

    メニュー名前受け渡し変更

--- Kansenzinrou/Assets/Script/Name/Name.cs
+++ Kansenzinrou/Assets/Script/Name/Name.cs
@@ -9,9 +9,8 @@ public class Name : MonoBehaviour
     public InputField inputField1;
     public InputField inputField2;
     public InputField inputField3;
-    private DeliverClass anotherScript;
-    GameObject anotherObject;
-
+    public string[] nameholde=new string [4];
+    public string gmnameholde;
     private void Start()
     {
         // DeliverClassを取得
@@ -26,34 +25,40 @@ public class Name : MonoBehaviour
     public void OnClickButton()
     {
         SceneManager.sceneLoaded += NamePass;
-        anotherObject = GameObject.Find("Deliver");
-         anotherScript = anotherObject.GetComponent<DeliverClass>();
         // DeliverClassを取得
         //DeliverClass deliver = FindObjectOfType<DeliverClass>();
         // InputFieldの文字列をDeliverに渡す
-        anotherScript.deliverString = inputField.text;
-        anotherScript.NamescenePN1 = inputField0.text;
-        anotherScript.NamescenePN2 = inputField1.text;
-        anotherScript.NamescenePN3 = inputField2.text;
-        anotherScript.NamescenePN4 = inputField3.text;
+        gmnameholde = inputField.ToString();
+        nameholde[0] = inputField0.ToString();
+        nameholde[1] = inputField1.ToString();
+        nameholde[2] = inputField2.ToString();
+        nameholde[3] = inputField3.ToString();
         // シーンを20190304_02に移動
         feadSC.fade("Main");
-        Debug.Log(anotherScript.deliverString);
-        Debug.Log(anotherScript.NamescenePN1);
-        Debug.Log(anotherScript.NamescenePN2);
-        Debug.Log(anotherScript.NamescenePN3);
-        Debug.Log(anotherScript.NamescenePN4);
+        Debug.Log(gmnameholde);
+        Debug.Log(nameholde[0]);
+        Debug.Log(nameholde[1]);
+        Debug.Log(nameholde[2]);
+        Debug.Log(nameholde[3]);
     }
 
     private void NamePass(Scene next, LoadSceneMode mode)
     {
         GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName = new string[4];
         GameObject.Find("Maneger").GetComponent<GetPlayerName>().GMName = "GMプレイヤーの名前ー";
+        GameObject.Find("Maneger").GetComponent<GetPlayerName>().GMName = inputField.ToString();
         for (int i = 0;i < 4; i++)
         {
             GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[i] = "プレイヤーの名前ー";
+            GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[i] = nameholde[i];
         }
-
+        for (int i = 0; i < GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName.Length; i++)
+        {
+            string tmp = GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[i];
+            int randomIndex = Random.Range(i, GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName.Length);
+            GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[i] = GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[randomIndex];
+            GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[randomIndex] = tmp;
+        }
         SceneManager.sceneLoaded -= NamePass;
     }
 }
