5740cb9faa34e5ca2fe83e0deae83250be7c6a8f
50arashikenta <50arashikenta@gmail.com>
2020-10-24 22:46:53 +0900

    メニューの背景変更

--- Kansenzinrou/Assets/Script/Name/Name.cs
+++ Kansenzinrou/Assets/Script/Name/Name.cs
@@ -37,7 +37,7 @@ public class Name : MonoBehaviour
         anotherScript.NamescenePN4 = inputField3.text;
         // シーンを20190304_02に移動
         SceneManager.LoadScene("Main");
-        feadSC.fade("Menu");
+        feadSC.fade("Main");
         Debug.Log(anotherScript.deliverString);
         Debug.Log(anotherScript.NamescenePN1);
         Debug.Log(anotherScript.NamescenePN2);
