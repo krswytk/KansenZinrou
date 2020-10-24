80b4e15dfed1e8a4cb73f70efc76769c386daf84
DESKTOP-31OM9TO\ひざがみ <krswytk.0228@gmail.com>
2020-10-24 20:46:45 +0900

    取引関係のスクリプトを改善 これで動くといいなー 残り　購入時のエラー削除 タイマーの音が起動していない

---
+++ Kansenzinrou/Assets/Script/Main/Next.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Next : MonoBehaviour
+{
+    BusinessALL BA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BA = GameObject.Find("Maneger").GetComponent<BusinessALL>();
+    }
+
+    public void NextSW()//交渉決定用のボタン
+    {
+        BA.NextSW();
+    }
+}
