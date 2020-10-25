34f138cc6f588e298beffe7fecfb683d7ed4fa8f
CurtanaOriginal <titan7312@gmail.com>
2020-10-25 22:11:57 +0900

    bgm,SE,シーン遷移以外カンセイ 背景挿入、その他微調整 リザルトより

---
+++ Kansenzinrou/Assets/Script/Result/Log.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Log : MonoBehaviour
+{
+    public Text LogText;
+    string[] stringLog;
+    // Start is called before the first frame update
+    void Start() {
+        LogText.text = "";
+       stringLog = InfrctionLog.getInfectedLog();
+        for (int i = 0; i < stringLog.Length; i++)
+            LogText.text += stringLog[i];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
