87413de0a8a44faf74820f8f39b89336095f10f7
50arashikenta <50arashikenta@gmail.com>
2020-11-09 19:12:50 +0900

    時間で取引先とターンの表示切替(未完成)

---
+++ Kansenzinrou/Assets/Script/Main/changeTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeTimer : MonoBehaviour
+{
+    [SerializeField] GameObject text;
+    bool txton = true;
+    [HideInInspector] public float span = 5f;
+    private float currentTime = 0f;
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if (currentTime > span)
+        {
+            if (txton == true) { 
+            text.SetActive(false);
+                txton = false;
+            }else if (txton == false)
+            {
+                text.SetActive(true);
+                txton = true;
+            }
+            Debug.LogFormat("{0}秒経過", span);
+            currentTime = 0f;
+        }
+    }
+}
