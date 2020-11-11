b15a962a770f41a48d7af592b0e300b35adf76d0
TakanoMai <shadowcat.game.mai@gmail.com>
2020-11-11 18:10:11 +0900

    マルチモードのシーン作った

---
+++ Kansenzinrou/Assets/Script/Multi/Multi.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Multi : MonoBehaviour
+{
+    public Text[] inputField;
+
+    public void OnClickButton()
+    {
+        Debug.Log(inputField[0].text.ToString());
+        Debug.Log(inputField[1].text.ToString());
+        SceneManager.sceneLoaded += NamePass;
+        feadSC.fade("Main");
+    }
+
+    private void NamePass(Scene next, LoadSceneMode mode)
+    {
+        GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName = new string[4];
+        GameObject.Find("Maneger").GetComponent<GetPlayerName>().GMName = inputField[0].text.ToString(); //ルームName？変数分からん
+        for (int i = 0; i < inputField.Length - 1; i++)
+        {
+            GameObject.Find("Maneger").GetComponent<GetPlayerName>().PlayerName[i] = inputField[i + 1].text.ToString();
+        }
+        SceneManager.sceneLoaded -= NamePass;
+    }
+}
